2146fa9a0971fdfa4db4ce7de47d281f8d646027
cquevedo61 <cquevedo@email.arizona.edu>
2017-09-11 09:52:40 -0700

    RED LEADER

--- Hello.java
+++ Hello.java
@@ -12,7 +12,7 @@ public class Hello {
         
         // Red Leader should add a print statement that says "Lock S-foils in attack position." 
         // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
-
+	System.out.println("Lock S-foils in attack position.")
     }
 
     public static void goldLeader() {
@@ -25,7 +25,7 @@ public class Hello {
     public static void redLeader() {
  
         // Red Leader should add a print statement that says "I copy, gold leader!"
-        
+        System.out.println("I copy, gold leader!")
 
     }
 
